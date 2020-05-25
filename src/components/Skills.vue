da423e9e26bd0309addd70c9ccd69d11b6c67144
Mahi Begoug <gm_begoug@esi.dz>
2020-05-25 01:45:40 +0100

    add transition to form

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,6 +4,10 @@
      
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill you have ..." v-model="skill" v-validate="'min:5'" name="skill" >
+      
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
       </form>
 
 
