59a6bb3625cd709a15a69c8ebb6030d98bbf6fed
Mahi Begoug <gm_begoug@esi.dz>
2020-05-25 01:32:59 +0100

    edit About component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,18 @@
 <template>
-    
+    <div class="about">
+        <h1>Hello {{their_name}}, this is an zbout page </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "About"
+        name: "About",
+        data(){
+            return {
+                their_name: this.$route.params.name
+            }
+        }
     }
 </script>
 
