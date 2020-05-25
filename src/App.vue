5079c0255ffe88d66a8a748c1585e4451e7f081f
Mahi Begoug <gm_begoug@esi.dz>
2020-05-25 01:48:17 +0100

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <Skills />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
 <script>
-import Skills from './components/Skills.vue'
 
 export default {
   name: 'app',
-  components: {
-    Skills
-  }
+  
 }
 </script>
 
